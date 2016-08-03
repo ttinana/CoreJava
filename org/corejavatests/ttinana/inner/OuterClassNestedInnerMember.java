87a43ba41585179509431f8b8731f7e48ee90c14
ttinana <ttinana@gmail.com>
2016-08-03 10:13:42 +0200

    .

--- org/corejavatests/ttinana/inner/OuterClassNestedInnerMember.java
+++ org/corejavatests/ttinana/inner/OuterClassNestedInnerMember.java
@@ -7,8 +7,36 @@ package org.corejavatests.ttinana.inner;
  * @author ttinana
  *
  */
-public class OuterClassNestedInnerMember {
+class OuterClassNestedInnerMember {
+	private String outerNoStatic ="outerNoStatic";
+	private String outerStatic = "outerStatic";
+	public static void main(String args[]) {
+	//An instance of InnerClass can exist only within an instance of OuterClass 
+	// and has direct access to the methods and fields of its enclosing instance.
+	OuterClassNestedInnerMember oco = new OuterClassNestedInnerMember();
+	
+	OuterClassNestedInnerMember.InnerClass innerObject = oco.new InnerClass();
+	System.out.println(innerObject.innerNoStatic);
+	System.out.println(innerObject.printOuterNoStaticFromInnerClass());
+	System.out.println("inner (therefore NO static class can not have static member or method)");
+	}
 	class InnerClass {
+		private String innerNoStatic ="innerNoStatic";
+
+		private String printOuterNoStaticFromInnerClass(){
+			OuterClassNestedInnerMember oco = new OuterClassNestedInnerMember();
+			return oco.outerNoStatic;
+		}
+		
+		/*  
+		 * static members or methods can only be declared in a static or top level type
+		 * 
+		 * inner static member can not exist inside inner no static class!!!
+		 because inner class can only exist as instance of outer
+		 thus below returns compile error:
+		 private static String innerStatic ="innerNoStatic";*/
+		
+		
 		
 	}
 
