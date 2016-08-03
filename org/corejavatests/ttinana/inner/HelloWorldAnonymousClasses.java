086db2aa13ebbf09c76a93afd359762574caf971
ttinana <ttinana@gmail.com>
2016-08-03 10:26:16 +0200

    added coment about serialization of inner classes

--- org/corejavatests/ttinana/inner/HelloWorldAnonymousClasses.java
+++ org/corejavatests/ttinana/inner/HelloWorldAnonymousClasses.java
@@ -21,6 +21,30 @@ scope and their enclosing instance and make that accessible.
  * */
 
 
+/**
+@see <a href= https://docs.oracle.com/javase/tutorial/java/javaOO/nested.html"> nested </a>
+*/
+
+/**
+ * Serialization
+
+Serialization of inner classes, including local and anonymous classes, 
+is strongly discouraged. When the Java compiler compiles certain constructs, 
+such as inner classes, it creates synthetic constructs; these are classes, 
+methods, fields, and other constructs that do not have a corresponding 
+construct in the source code. Synthetic constructs enable Java compilers to 
+implement new Java language features without changes to the JVM. However, 
+synthetic constructs can vary among different Java compiler implementations, 
+which means that .class files can vary among different implementations as well. 
+Consequently, you may have compatibility issues if you serialize an inner class 
+and then deserialize it with a different JRE implementation. 
+See the section Implicit and Synthetic Parameters in the section Obtaining 
+Names of Method Parameters 
+for more information about the synthetic constructs generated when an inner 
+class is compiled.
+ */
+
+
 public class HelloWorldAnonymousClasses {
 
 	interface HelloWorld {
